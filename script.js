7e688f57fb8af227c08ee41bc125fc8845c9b7dd
Elise <elise@MBP-van-Elise.lan>
2019-09-03 10:31:27 +0200

    basic working pokedex with full names image index and moves

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 const input = document.getElementById("poke-search");
 const searchButton = document.getElementById("search-button");
-const imageScreen = document.getElementById("display");
+const pokeNumberBox = document.getElementById("poke-number");
 
 let nameBox = document.getElementById("name");
 let movesBox = document.getElementById("moves");
@@ -19,18 +19,34 @@ const pokeData = fetchData("https://pokeapi.co/api/v2/generation/1/");
 const xhttp = new XMLHttpRequest();
 
 const pokeSpecies = pokeData.pokemon_species;
-console.log(pokeSpecies);
 
 searchButton.addEventListener('click', () => {
     const results = pokeSpecies.filter((specimen)=> {
         //return name.includes(input.value);
-        return specimen.name === input.value;
+        const lowercaseInput = input.value.toLowerCase();
+        return specimen.name === lowercaseInput;
     });
 
     nameBox.innerHTML = results[0].name;
     input.value="";
 
     const pokeSpeciesData = fetchData(`https://pokeapi.co/api/v2/pokemon/${results[0].name}`);
-    console.log(pokeSpeciesData);
 
+    movesBox.innerHTML = "";
+    const list = document.createElement('ul');
+    movesBox.append(list);
+    pokeSpeciesData.moves.map((move) => {
+        const listEl = document.createElement('li');
+        listEl.innerHTML = move.move.name;
+        list.append(listEl);
+    }
+    )
+    console.log(pokeSpeciesData.sprites.front_default);
+    const imageScreen = document.getElementById("displayImage");
+    imageScreen.src = pokeSpeciesData.sprites.front_default;
+    imageScreen.alt = `${results[0].name}-image`;
+
+    const gameIndex = pokeSpeciesData.game_indices[4].game_index;
+    
+    pokeNumberBox.querySelector('p').innerHTML = gameIndex;
 });
